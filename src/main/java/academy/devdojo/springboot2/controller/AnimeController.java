da82090b5cd7a221c12bfd6380538aba24711fce
Samuel Almeida <barbosasamuel24@gmail.com>
2022-02-28 12:12:49 -0300

    Code refactor

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -4,7 +4,6 @@ import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 
-
 @RestController
-@RequestMapping("Anime")
+@RequestMapping("anime")
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
@@ -24,12 +22,7 @@ public class AnimeController {
     @GetMapping(path = "list")
     public List<Anime> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"), new Anime("Naruto"));
+        return List.of(new Anime("Boku No Hero"), new Anime("Berserk"));
     }
 
-    @GetMapping(path = "list2")
-    public List<Anime> list2(){
-        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"), new Anime("Naruto"));
-    }
 }
